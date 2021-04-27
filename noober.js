8ba4fea99c61e9c9036f3c8d4f3e6b2d8e43024e
michaelsit <81634553+michaelsit@users.noreply.github.com>
2021-04-26 23:38:10 -0500

    commit

--- noober.js
+++ noober.js
@@ -32,10 +32,43 @@ window.addEventListener('DOMContentLoaded', async function() {
       let dropoffAdd = `${dropoff.address}`
       let dropoffCity = `${dropoff.city}, ${dropoff.state} ${dropoff.zip}`
     }
-    
+  
 
-  //   // Create variable for HTML element to add
-  //   let rides = document.querySelector(`.rides`)
+  //Create variable for HTML element to add
+  let rides = document.querySelector(`.rides`)
+
+  // insert HTML into page w JS
+  rides.insertAdjacentHTML(`beforeend`,`
+  <h1 class="inline-block mt-8 px-4 py-2 rounded-xl text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+    <i class="fas fa-car-side"></i>
+    <span>Noober ${rideStatus}</span>
+  </h1>
+  <div class="border-4 border-gray-900 p-4 my-4 text-left">
+    <div class="flex">
+      <div class="w-1/2">
+        <h2 class="text-2xl py-1">${name}</h2>
+        <p class="font-bold text-gray-600">${details.phoneNumber}</p>
+      </div>
+      <div class="w-1/2 text-right">
+        <span class="rounded-xl bg-gray-600 text-white p-2">
+          ${numOfPass} passengers
+        </span>
+      </div>
+    </div>
+    <div class="mt-4 flex">
+      <div class="w-1/2">
+        <div class="text-sm font-bold text-gray-600">PICKUP</div>
+        <p>${pickupAdd1}</p>
+        <p>${pickupAdd2}</p>
+      </div>
+      <div class="w-1/2">
+        <div class="text-sm font-bold text-gray-600">DROPOFF</div>
+        <p>${dropoffAdd1}</p>
+        <p>${dropoffAdd2}</p>
+      </div>
+    </div>
+  </div>
+`)
 
 
 
