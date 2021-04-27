75304267b0e40486586fcf31e4dc6b3cab6f17f6
michaelsit <81634553+michaelsit@users.noreply.github.com>
2021-04-26 23:18:16 -0500

    commit

--- noober.js
+++ noober.js
@@ -8,38 +8,38 @@ window.addEventListener('DOMContentLoaded', async function() {
   // 🔥 start here: write the recipe (algorithm), then write the code
   
   // loop through the rides data
-  for (let i = 0; i < json.length; i++) {
-    // create variable to store each ride in memory
-    // define ride types for pupleRequested (purple or X) and numberOfPassengers > 3 for XL
-    let purpleReq = json[i].purpleRequested
-    let numberOfPass = json[i].numberOfPassengers
-    let rideType
-    // Create ride type conditional based on # of passengers, purple, ect. and store it in memory
-      if (purpleReq == true) {
-        rideType = `Noober Purple` 
-      } else if (numberOfPass > 3) {
-        rideType = `Noober XL`
-      } else {
-        rideType = `Noober X`
-      }
-    // passenger detail objects (First, Last, Phone #)
-    let pass = json[i].passengerDetails
-      let passName = `${pass.first} ${pass.last}`
-    // define pickup & dropoff objects (Address, City, State Zip)
-    let pickup = json[i].pickupLocation
-      let pickupAdd = `${pickup.address}`
-      let pickupCity = `${pickup.city}, ${pickup.state} ${pickup.zip}`
-    let dropoff = json[i].dropoffLocation
-      let dropoffAdd = `${dropoff.address}`
-      let dropoffCity = `${dropoff.city}, ${dropoff.state} ${dropoff.zip}`
+  // for (let i = 0; i < json.length; i++) {
+  //   // create variable to store each ride in memory
+  //   // define ride types for pupleRequested (purple or X) and numberOfPassengers > 3 for XL
+  //   let purpleReq = json[i].purpleRequested
+  //   let numberOfPass = json[i].numberOfPassengers
+  //   let rideType
+  //   // Create ride type conditional based on # of passengers, purple, ect. and store it in memory
+  //     if (purpleReq == true) {
+  //       rideType = `Noober Purple` 
+  //     } else if (numberOfPass > 3) {
+  //       rideType = `Noober XL`
+  //     } else {
+  //       rideType = `Noober X`
+  //     }
+  //   // passenger detail objects (First, Last, Phone #)
+  //   let pass = json[i].passengerDetails
+  //     let passName = `${pass.first} ${pass.last}`
+  //   // define pickup & dropoff objects (Address, City, State Zip)
+  //   let pickup = json[i].pickupLocation
+  //     let pickupAdd = `${pickup.address}`
+  //     let pickupCity = `${pickup.city}, ${pickup.state} ${pickup.zip}`
+  //   let dropoff = json[i].dropoffLocation
+  //     let dropoffAdd = `${dropoff.address}`
+  //     let dropoffCity = `${dropoff.city}, ${dropoff.state} ${dropoff.zip}`
     
 
-    // Create variable for HTML element to add
-    let rides = document.querySelector(`.rides`)
+  //   // Create variable for HTML element to add
+  //   let rides = document.querySelector(`.rides`)
 
 
 
-  }
+  // }
 
 
   // sample data string from json
